bfbe2048385451271ce998d72276297d60bd7a62
ldabudiab <ldabudiab@gmail.com>
2022-01-18 15:07:48 -0600

    userlist now available

--- Tabloid/client/src/components/Header.js
+++ Tabloid/client/src/components/Header.js
@@ -27,6 +27,13 @@ export default function Header({ isLoggedIn }) {
               <NavItem>
                 <NavLink tag={RRNavLink} to="/">Home</NavLink>
               </NavItem>
+              
+            }
+            {isLoggedIn &&
+              <NavItem>
+                <NavLink tag={RRNavLink} to="/users">Users</NavLink>
+              </NavItem>
+              
             }
           </Nav>
           <Nav navbar>
